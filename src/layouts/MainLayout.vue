d97fdca2fa6484d7b1e72b6b10054899ae5db989
Moustafa Shaaban <Moustafa_Shaaban@zohomail.com>
2025-03-14 13:59:09 +0200

    Persisted the dark mode preference using localStorage and updated AboutView page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,26 +3,24 @@
 
         <q-header reveal elevated class="bg-primary text-white" height-hint="98">
             <q-toolbar>
-                <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+                <!-- <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" /> -->
 
                 <q-toolbar-title>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-                    </q-avatar>
-                    Title
+                    Quasar CRUD
                 </q-toolbar-title>
+                <q-space />
+                <q-btn flat round :icon="$q.dark.isActive ? 'light_mode' : 'dark_mode'" @click="toggleTheme" />
             </q-toolbar>
 
             <q-tabs align="left">
                 <q-route-tab to="/" label="Home" />
                 <q-route-tab to="/about" label="About" />
-                <q-route-tab @click="toggleTheme" label="Toggle Theme" />
+                <!-- <q-route-tab @click="toggleTheme" label="Toggle Theme" /> -->
             </q-tabs>
         </q-header>
 
-        <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
-            <!-- drawer content -->
-        </q-drawer>
+        <!-- <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
+        </q-drawer> -->
 
         <q-page-container>
             <router-view />
@@ -30,8 +28,20 @@
 
     </q-layout>
 </template>
-  
-<script>
+
+<script setup>
+import { Dark } from 'quasar'
+
+const darkMode = localStorage.getItem('darkMode') === 'true';
+Dark.set(darkMode);
+
+function toggleTheme() {
+    Dark.toggle()
+    localStorage.setItem('darkMode', Dark.isActive);
+}
+</script>
+
+<!-- <script>
 import { Dark } from 'quasar'
 import { ref } from 'vue'
 
@@ -50,4 +60,4 @@ export default {
         }
     }
 }
-</script>
+</script> -->
